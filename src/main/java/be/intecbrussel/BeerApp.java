665a887137ee8446f9203b7bdcc76d012eb9da26
Daniel Magyar <magyard90@yahoo.fr>
2020-01-20 16:02:15 +0100

    commit

--- src/main/java/be/intecbrussel/BeerApp.java
+++ src/main/java/be/intecbrussel/BeerApp.java
@@ -4,32 +4,51 @@ import be.intecbrussel.model.Beer;
 import be.intecbrussel.model.BeerOrder;
 import be.intecbrussel.service.BeerOrderRepository;
 import be.intecbrussel.service.BeerRepository;
+import be.intecbrussel.service.BeerRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BeerApp {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(BeerApp.class, args);
 
+
+        BeerRepositoryImpl beerRepository = context.getBean("beerRepository", BeerRepositoryImpl.class);
+
+
+        Beer beer = new Beer();
+        beer.setId(4);
+        beer.setName("TheNewComer");
+
+        beerRepository.updateBeer(beer);
+
         /*
-        BeerRepository beerRepository = context.getBean("beerRepository", BeerRepository.class);
-        Beer beer = beerRepository.getBeerById(20);
-        System.out.println(beer);
-         */
+        List<Beer> list = beerRepository.getBeerByAlchol(0);
+        list.forEach(b -> System.out.println(b.getName()));
+        */
+
 
         /*
         BeerOrderRepository beerOrderRepository = context.getBean("beerorderrepository", BeerOrderRepository.class);
+        //cannot use context.getBean() to create a BeerOrder instance. BeerOrder is an entity but not a bean
         BeerOrder beerOrder1 = new BeerOrder();
-        beerOrder1.setId(2);
-        beerOrder1.setName("Danny De Vos");
+        beerOrder1.setId(3);
+        beerOrder1.setName("Femke van Hoogstaal");
         beerOrderRepository.saveOrder(beerOrder1);
-         */
+        */
 
+
+
+        /*
         BeerOrderRepository beerOrderRepository = context.getBean("beerorderrepository", BeerOrderRepository.class);
-        BeerOrder beerOrder = beerOrderRepository.getBeerOrderById(1);
+        BeerOrder beerOrder = beerOrderRepository.getBeerOrderById(2);
         System.out.println(beerOrder.getName());
+        */
+
 
 
     }
--- src/main/java/be/intecbrussel/service/BeerRepositoryImpl.java
+++ src/main/java/be/intecbrussel/service/BeerRepositoryImpl.java
@@ -3,10 +3,7 @@ package be.intecbrussel.service;
 import be.intecbrussel.model.Beer;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.*;
 import java.util.List;
 
 
@@ -41,6 +38,47 @@ public class BeerRepositoryImpl implements BeerRepository {
             }
         }
         */
+    }
+
+    public List<Beer> getBeerByAlchol(float alcohol) {
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Beer> query = em.createQuery("select b from Beer b where b.alcohol =?1", Beer.class);
+
+        query.setParameter(1, alcohol);
+
+        return query.getResultList();
+    }
+
+    public void updateBeer (Beer b) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction txn = em.getTransaction();
+
+        try {
+
+            txn.begin();
+
+            Beer beerToBeModified = em.find(Beer.class, b.getId());
+
+            beerToBeModified.setAlcohol(b.getAlcohol());
+            beerToBeModified.setBrewer(b.getBrewer());
+            beerToBeModified.setCategory(b.getCategory());
+            beerToBeModified.setName(b.getName());
+            beerToBeModified.setPrice(b.getPrice());
+            beerToBeModified.setStock(b.getStock());
+            beerToBeModified.setVersion(b.getVersion());
+
+            txn.commit();
+        }
+
+        catch (Exception e) {
+            txn.rollback();
+            throw e;
+        }
+
+
+
+
+
     }
 
 
